c47980272c3f98e35e8f59c62d67ab109e1780dc
Lei Xue <carmark.dlut@gmail.com>
2015-08-07 15:23:44 +0800

    return os.ErrNotExist for qemu and xen 's network

--- hypervisor/qemu/network.go
+++ hypervisor/qemu/network.go
@@ -2,22 +2,21 @@ package qemu
 
 import (
 	"os"
-	"fmt"
 
 	"github.com/hyperhq/runv/hypervisor/pod"
 	"github.com/hyperhq/runv/hypervisor/network"
 )
 
 func (qd *QemuDriver) InitNetwork(bIface, bIP string) error {
-	return fmt.Errorf("Please use generic network driver")
+	return os.ErrNotExist
 }
 
 func (qc *QemuContext) AllocateNetwork(vmId, requestedIP string,
 		maps []pod.UserContainerPort) (*network.Settings, error) {
-	return nil, fmt.Errorf("Please use generic network driver")
+	return nil, os.ErrNotExist
 }
 
 func (qc *QemuContext) ReleaseNetwork(vmId, releasedIP string, maps []pod.UserContainerPort,
 				file *os.File) error {
-	return fmt.Errorf("Please use generic network driver")
+	return os.ErrNotExist
 }
--- hypervisor/xen/network.go
+++ hypervisor/xen/network.go
@@ -2,22 +2,21 @@ package xen
 
 import (
 	"os"
-	"fmt"
 
 	"github.com/hyperhq/runv/hypervisor/pod"
 	"github.com/hyperhq/runv/hypervisor/network"
 )
 
 func (xd *XenDriver) InitNetwork(bIface, bIP string) error {
-	return fmt.Errorf("Please use generic network driver")
+	return os.ErrNotExist
 }
 
 func (xc *XenContext) AllocateNetwork(vmId, requestedIP string,
 		maps []pod.UserContainerPort) (*network.Settings, error) {
-	return nil, fmt.Errorf("Please use generic network driver")
+	return nil, os.ErrNotExist
 }
 
 func (xc *XenContext) ReleaseNetwork(vmId, releasedIP string, maps []pod.UserContainerPort,
 				file *os.File) error {
-	return fmt.Errorf("Please use generic network driver")
+	return os.ErrNotExist
 }
