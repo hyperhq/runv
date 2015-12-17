484f6a8a1818822dc6d01b61adb7f0c7b4ad6803
Gao feng <omarapazanadi@gmail.com>
2015-12-17 12:07:06 +0800

    Don't create tap device for libvirt domain
    Signed-off-by: Gao feng <omarapazanadi@gmail.com>

--- hypervisor/libvirt/network.go
+++ hypervisor/libvirt/network.go
@@ -17,15 +17,15 @@ func (ld *LibvirtDriver) InitNetwork(bIface, bIP string) error {
 
 func (lc *LibvirtContext) ConfigureNetwork(vmId, requestedIP string,
 	maps []pod.UserContainerPort, config pod.UserInterface) (*network.Settings, error) {
-	return nil, nil
+	return network.Configure(vmId, requestedIP, true, maps, config)
 }
 
 func (lc *LibvirtContext) AllocateNetwork(vmId, requestedIP string,
 	maps []pod.UserContainerPort) (*network.Settings, error) {
-	return nil, nil
+	return network.Allocate(vmId, requestedIP, true, maps)
 }
 
 func (lc *LibvirtContext) ReleaseNetwork(vmId, releasedIP string, maps []pod.UserContainerPort,
 	file *os.File) error {
-	return nil
+	return network.Release(vmId, releasedIP, maps, nil)
 }
