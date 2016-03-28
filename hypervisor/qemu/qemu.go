c4af8a41d45e182e0d5af1561c70c118e0cdcde3
Lai Jiangshan <jiangshanlai@gmail.com>
2016-03-28 18:08:36 +0800

    support template on qemu driver
    Signed-off-by: Lai Jiangshan <jiangshanlai@gmail.com>

--- hypervisor/qemu/qemu.go
+++ hypervisor/qemu/qemu.go
@@ -315,14 +315,14 @@ func (qc *QemuContext) arguments(ctx *hypervisor.VmContext) []string {
 	qc.cpus = boot.CPU
 
 	var machineClass, memParams, cpuParams string
-	if ctx.Boot.HotAddCpuMem {
+	if boot.HotAddCpuMem || boot.BootToBeTemplate || boot.BootFromTemplate {
 		machineClass = "pc-i440fx-2.1"
-		memParams = fmt.Sprintf("size=%d,slots=1,maxmem=%dM", ctx.Boot.Memory, hypervisor.DefaultMaxMem) // TODO set maxmem to the total memory of the system
-		cpuParams = fmt.Sprintf("cpus=%d,maxcpus=%d", ctx.Boot.CPU, hypervisor.DefaultMaxCpus)           // TODO set it to the cpus of the system
+		memParams = fmt.Sprintf("size=%d,slots=1,maxmem=%dM", boot.Memory, hypervisor.DefaultMaxMem) // TODO set maxmem to the total memory of the system
+		cpuParams = fmt.Sprintf("cpus=%d,maxcpus=%d", boot.CPU, hypervisor.DefaultMaxCpus)           // TODO set it to the cpus of the system
 	} else {
 		machineClass = "pc-i440fx-2.0"
-		memParams = strconv.Itoa(ctx.Boot.Memory)
-		cpuParams = strconv.Itoa(ctx.Boot.CPU)
+		memParams = strconv.Itoa(boot.Memory)
+		cpuParams = strconv.Itoa(boot.CPU)
 	}
 
 	params := []string{
@@ -348,11 +348,27 @@ func (qc *QemuContext) arguments(ctx *hypervisor.VmContext) []string {
 			"-kernel", boot.Kernel, "-initrd", boot.Initrd, "-append", "console=ttyS0 panic=1 no_timer_check")
 	}
 
-	return append(params,
+	params = append(params,
 		"-realtime", "mlock=off", "-no-user-config", "-nodefaults", "-no-hpet",
 		"-rtc", "base=utc,driftfix=slew", "-no-reboot", "-display", "none", "-boot", "strict=on",
-		"-m", memParams, "-smp", cpuParams,
-		"-qmp", fmt.Sprintf("unix:%s,server,nowait", qc.qmpSockName), "-serial", fmt.Sprintf("unix:%s,server,nowait", ctx.ConsoleSockName),
+		"-m", memParams, "-smp", cpuParams)
+
+	if boot.BootToBeTemplate || boot.BootFromTemplate {
+		memObject := fmt.Sprintf("memory-backend-file,id=hyper-template-memory,size=%dM,mem-path=%s", boot.Memory, boot.MemoryPath)
+		if boot.BootToBeTemplate {
+			memObject = memObject + ",share=on"
+		}
+		nodeConfig := fmt.Sprintf("node,nodeid=0,cpus=0-%d,memdev=hyper-template-memory", hypervisor.DefaultMaxCpus-1)
+		params = append(params, "-object", memObject, "-numa", nodeConfig)
+		if boot.BootFromTemplate {
+			params = append(params, "-incoming", fmt.Sprintf("exec:cat %s", boot.DevicesStatePath))
+		}
+	} else if boot.HotAddCpuMem {
+		nodeConfig := fmt.Sprintf("node,nodeid=0,cpus=0-%d,mem=%d", hypervisor.DefaultMaxCpus-1, boot.Memory)
+		params = append(params, "-numa", nodeConfig)
+	}
+
+	return append(params, "-qmp", fmt.Sprintf("unix:%s,server,nowait", qc.qmpSockName), "-serial", fmt.Sprintf("unix:%s,server,nowait", ctx.ConsoleSockName),
 		"-device", "virtio-serial-pci,id=virtio-serial0,bus=pci.0,addr=0x2", "-device", "virtio-scsi-pci,id=scsi0,bus=pci.0,addr=0x3",
 		"-chardev", fmt.Sprintf("socket,id=charch0,path=%s,server,nowait", ctx.HyperSockName),
 		"-device", "virtserialport,bus=virtio-serial0.0,nr=1,chardev=charch0,id=channel0,name=sh.hyper.channel.0",
