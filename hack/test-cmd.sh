0f371fcb38e544eda98e7821d44ff0a539878d44
Lai Jiangshan <jiangshanlai@gmail.com>
2016-04-19 00:14:53 +0800

    update the github address of the hyperd
    Signed-off-by: Lai Jiangshan <jiangshanlai@gmail.com>

--- hack/test-cmd.sh
+++ hack/test-cmd.sh
@@ -20,7 +20,7 @@ function cancel_and_exit()
 	exit 0 # don't fail in preparing hyper
 }
 
-cd ${GOPATH}/src/github.com/hyperhq/hyper || cancel_and_exit "failed to find hyper"
+cd ${GOPATH}/src/github.com/hyperhq/hyperd || cancel_and_exit "failed to find hyper"
 ./autogen.sh || cancel_and_exit "failed to autogen hyper"
 ./configure || cancel_and_exit "failed to configure hyper"
 make || cancel_and_exit "failed to compile hyper"
