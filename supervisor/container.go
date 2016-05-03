baf0ed20de200ba94b51cb59ce1e57be1d3fabbb
Zhang Wei <zhangwei555@huawei.com>
2016-05-03 19:58:29 +0800

    Add volume support
    Currently runv don't support docker volumes(OCI Mounts fields), this
    commit will add volume support, so we can bind mount host dir or make a
    volume dir in container.
    
    Signed-off-by: Zhang Wei <zhangwei555@huawei.com>

--- supervisor/container.go
+++ supervisor/container.go
@@ -8,8 +8,11 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"time"
 
+	"github.com/docker/docker/pkg/mount"
+	"github.com/docker/docker/pkg/symlink"
 	"github.com/golang/glog"
 	"github.com/hyperhq/runv/hypervisor"
 	"github.com/hyperhq/runv/hypervisor/pod"
@@ -80,10 +83,18 @@ func (c *Container) run(p *Process) error {
 	vmRootfs := filepath.Join(hypervisor.BaseDir, c.ownerPod.vm.Id, hypervisor.ShareDirTag, c.Id, "rootfs")
 	os.MkdirAll(vmRootfs, 0755)
 
+	// Mount rootfs
 	err = utils.Mount(u.Image, vmRootfs, c.Spec.Root.Readonly)
 	if err != nil {
 		glog.V(1).Infof("mount %s to %s failed: %s\n", u.Image, vmRootfs, err.Error())
 		return err
+	}
+
+	// Mount necessary files and directories from spec
+	for _, m := range c.Spec.Mounts {
+		if err := mountToRootfs(&m, vmRootfs, ""); err != nil {
+			return fmt.Errorf("mounting %q to rootfs %q failed", m.Destination, vmRootfs)
+		}
 	}
 
 	envs := make(map[string]string)
@@ -251,4 +262,110 @@ func (c *Container) reap() {
 	utils.Umount(filepath.Join(containerSharedDir, "rootfs"))
 	os.RemoveAll(containerSharedDir)
 	os.RemoveAll(filepath.Join(c.ownerPod.sv.StateDir, c.Id))
+}
+
+func mountToRootfs(m *specs.Mount, rootfs, mountLabel string) error {
+	// TODO: we don't use mountLabel here because it looks like mountLabel is
+	// only significant when SELinux is enabled.
+	var (
+		dest = m.Destination
+	)
+	if !strings.HasPrefix(dest, rootfs) {
+		dest = filepath.Join(rootfs, dest)
+	}
+
+	switch m.Type {
+	case "proc", "sysfs", "mqueue", "tmpfs", "cgroup":
+		glog.V(3).Infof("Skip mount point %q of type %s", m.Destination, m.Type)
+		return nil
+	case "bind":
+		stat, err := os.Stat(m.Source)
+		if err != nil {
+			// error out if the source of a bind mount does not exist as we will be
+			// unable to bind anything to it.
+			return err
+		}
+		// ensure that the destination of the bind mount is resolved of symlinks at mount time because
+		// any previous mounts can invalidate the next mount's destination.
+		// this can happen when a user specifies mounts within other mounts to cause breakouts or other
+		// evil stuff to try to escape the container's rootfs.
+		if dest, err = symlink.FollowSymlinkInScope(filepath.Join(rootfs, m.Destination), rootfs); err != nil {
+			return err
+		}
+		if err := checkMountDestination(rootfs, dest); err != nil {
+			return err
+		}
+		// update the mount with the correct dest after symlinks are resolved.
+		m.Destination = dest
+		if err := createIfNotExists(dest, stat.IsDir()); err != nil {
+			return err
+		}
+		if err := mount.Mount(m.Source, dest, m.Type, strings.Join(m.Options, ",")); err != nil {
+			return err
+		}
+	default:
+		if err := os.MkdirAll(dest, 0755); err != nil {
+			return err
+		}
+		return mount.Mount(m.Source, dest, m.Type, strings.Join(m.Options, ","))
+	}
+	return nil
+}
+
+// checkMountDestination checks to ensure that the mount destination is not over the top of /proc.
+// dest is required to be an abs path and have any symlinks resolved before calling this function.
+func checkMountDestination(rootfs, dest string) error {
+	invalidDestinations := []string{
+		"/proc",
+	}
+	// White list, it should be sub directories of invalid destinations
+	validDestinations := []string{
+		// These entries can be bind mounted by files emulated by fuse,
+		// so commands like top, free displays stats in container.
+		"/proc/cpuinfo",
+		"/proc/diskstats",
+		"/proc/meminfo",
+		"/proc/stat",
+		"/proc/net/dev",
+	}
+	for _, valid := range validDestinations {
+		path, err := filepath.Rel(filepath.Join(rootfs, valid), dest)
+		if err != nil {
+			return err
+		}
+		if path == "." {
+			return nil
+		}
+	}
+	for _, invalid := range invalidDestinations {
+		path, err := filepath.Rel(filepath.Join(rootfs, invalid), dest)
+		if err != nil {
+			return err
+		}
+		if path == "." || !strings.HasPrefix(path, "..") {
+			return fmt.Errorf("%q cannot be mounted because it is located inside %q", dest, invalid)
+		}
+
+	}
+	return nil
+}
+
+// createIfNotExists creates a file or a directory only if it does not already exist.
+func createIfNotExists(path string, isDir bool) error {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			if isDir {
+				return os.MkdirAll(path, 0755)
+			}
+			if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+				return err
+			}
+			f, err := os.OpenFile(path, os.O_CREATE, 0755)
+			if err != nil {
+				return err
+			}
+			f.Close()
+		}
+	}
+	return nil
 }
