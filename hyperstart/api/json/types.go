5cf1ae1c21a290a33d4abe94cc2d35f13678d122
YaoZengzeng <461941420@qq.com>
2016-05-15 19:11:52 +0800

    mv ttyMessage in hypervisor/tty.go to hyperstart/api/json/types.go
    Signed-off-by: Yao Zengzeng <461941420@qq.com>

---
+++ hyperstart/api/json/types.go
@@ -0,0 +1,19 @@
+package json
+
+import (
+	"encoding/binary"
+)
+
+type TtyMessage struct {
+	Session uint64
+	Message []byte
+}
+
+func (tm *TtyMessage) ToBuffer() []byte {
+	length := len(tm.Message) + 12
+	buf := make([]byte, length)
+	binary.BigEndian.PutUint64(buf[:8], tm.Session)
+	binary.BigEndian.PutUint32(buf[8:12], uint32(length))
+	copy(buf[12:], tm.Message)
+	return buf
+}
