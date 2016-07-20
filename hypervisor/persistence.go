ca64663b66e58f22d25fde75cd9b21533817634b
Xu Wang <gnawux@gmail.com>
2016-07-20 20:38:54 +0800

    REFACTOR:  disable persist temporarily
    Signed-off-by: Xu Wang <gnawux@gmail.com>

--- hypervisor/persistence.go
+++ hypervisor/persistence.go
@@ -1,10 +1,9 @@
 package hypervisor
 
 import (
-	"encoding/json"
-	"errors"
 	"sync"
 
+	"encoding/json"
 	"github.com/golang/glog"
 	hyperstartapi "github.com/hyperhq/runv/hyperstart/api/json"
 	"github.com/hyperhq/runv/hypervisor/pod"
@@ -51,39 +50,42 @@ func (ctx *VmContext) dump() (*PersistInfo, error) {
 		//UserSpec:    ctx.userSpec,
 		//VmSpec:      ctx.vmSpec,
 		HwStat:      ctx.dumpHwInfo(),
-		VolumeList:  make([]*PersistVolumeInfo, len(ctx.devices.imageMap)+len(ctx.devices.volumeMap)),
-		NetworkList: make([]*PersistNetworkInfo, len(ctx.devices.networkMap)),
+		//VolumeList:  make([]*PersistVolumeInfo, len(ctx.devices.imageMap)+len(ctx.devices.volumeMap)),
+		//NetworkList: make([]*PersistNetworkInfo, len(ctx.devices.networkMap)),
 	}
 
-	vid := 0
-	for _, image := range ctx.devices.imageMap {
-		info.VolumeList[vid] = image.info.dump()
-		info.VolumeList[vid].Containers = []int{image.pos}
-		info.VolumeList[vid].MontPoints = []string{"/"}
-		vid++
-	}
-
-	for _, vol := range ctx.devices.volumeMap {
-		info.VolumeList[vid] = vol.info.dump()
-		for idx, mount := range vol.pos {
-			for _, mp := range mount {
-				info.VolumeList[vid].Containers = append(info.VolumeList[vid].Containers, idx)
-				info.VolumeList[vid].MontPoints = append(info.VolumeList[vid].MontPoints, mp)
-			}
-		}
-		vid++
-	}
-
-	nid := 0
-	for _, nic := range ctx.devices.networkMap {
-		info.NetworkList[nid] = &PersistNetworkInfo{
-			Index:      nic.Index,
-			PciAddr:    nic.PCIAddr,
-			DeviceName: nic.DeviceName,
-			IpAddr:     nic.IpAddr,
-		}
-		nid++
-	}
+	//vid := 0
+	//for _, image := range ctx.devices.imageMap {
+	//	info.VolumeList[vid] = image.info.dump()
+	//	info.VolumeList[vid].Containers = []int{image.pos}
+	//	info.VolumeList[vid].MontPoints = []string{"/"}
+	//	vid++
+	//}
+	//
+	//for _, vol := range ctx.devices.volumeMap {
+	//	info.VolumeList[vid] = vol.info.dump()
+	//	mps := len(vol.pos)
+	//	info.VolumeList[vid].Containers = make([]int, mps)
+	//	info.VolumeList[vid].MontPoints = make([]string, mps)
+	//	i := 0
+	//	for idx, mp := range vol.pos {
+	//		info.VolumeList[vid].Containers[i] = idx
+	//		info.VolumeList[vid].MontPoints[i] = mp
+	//		i++
+	//	}
+	//	vid++
+	//}
+	//
+	//nid := 0
+	//for _, nic := range ctx.devices.networkMap {
+	//	info.NetworkList[nid] = &PersistNetworkInfo{
+	//		Index:      nic.Index,
+	//		PciAddr:    nic.PCIAddr,
+	//		DeviceName: nic.DeviceName,
+	//		IpAddr:     nic.IpAddr,
+	//	}
+	//	nid++
+	//}
 
 	return info, nil
 }
@@ -148,46 +150,46 @@ func (pinfo *PersistInfo) vmContext(hub chan VmEvent, client chan *types.VmRespo
 		return nil, err
 	}
 
-	ctx.vmSpec = pinfo.VmSpec
-	ctx.userSpec = pinfo.UserSpec
-	ctx.wg = wg
+	//ctx.vmSpec = pinfo.VmSpec
+	//ctx.userSpec = pinfo.UserSpec
+	//ctx.wg = wg
 
 	ctx.loadHwStatus(pinfo)
 
-	for _, vol := range pinfo.VolumeList {
-		binfo := vol.blockInfo()
-		if len(vol.Containers) != len(vol.MontPoints) {
-			return nil, errors.New("persistent data corrupt, volume info mismatch")
-		}
-		if len(vol.MontPoints) == 1 && vol.MontPoints[0] == "/" {
-			img := &imageInfo{
-				info: binfo,
-				pos:  vol.Containers[0],
-			}
-			ctx.devices.imageMap[vol.Name] = img
-		} else {
-			v := &volume{
-				info:     binfo,
-				pos:      make(map[int][]string),
-				readOnly: make(map[int][]bool),
-			}
-			for i := 0; i < len(vol.Containers); i++ {
-				idx := vol.Containers[i]
-				v.pos[idx] = append(v.pos[idx], vol.MontPoints[i])
-				v.readOnly[idx] = append(v.readOnly[idx], ctx.vmSpec.Containers[idx].RoLookup(vol.MontPoints[i]))
-			}
-			ctx.devices.volumeMap[vol.Name] = v
-		}
-	}
-
-	for _, nic := range pinfo.NetworkList {
-		ctx.devices.networkMap[nic.Index] = &InterfaceCreated{
-			Index:      nic.Index,
-			PCIAddr:    nic.PciAddr,
-			DeviceName: nic.DeviceName,
-			IpAddr:     nic.IpAddr,
-		}
-	}
+	//for _, vol := range pinfo.VolumeList {
+	//	binfo := vol.blockInfo()
+	//	if len(vol.Containers) != len(vol.MontPoints) {
+	//		return nil, errors.New("persistent data corrupt, volume info mismatch")
+	//	}
+	//	if len(vol.MontPoints) == 1 && vol.MontPoints[0] == "/" {
+	//		img := &imageInfo{
+	//			info: binfo,
+	//			pos:  vol.Containers[0],
+	//		}
+	//		ctx.devices.imageMap[vol.Name] = img
+	//	} else {
+	//		v := &volume{
+	//			info:     binfo,
+	//			pos:      make(map[int]string),
+	//			readOnly: make(map[int][]bool),
+	//		}
+	//		for i := 0; i < len(vol.Containers); i++ {
+	//			idx := vol.Containers[i]
+	//			v.pos[idx] = vol.MontPoints[i]
+	//			v.readOnly[idx] = ctx.vmSpec.Containers[idx].RoLookup(vol.MontPoints[i])
+	//		}
+	//		ctx.devices.volumeMap[vol.Name] = v
+	//	}
+	//}
+	//
+	//for _, nic := range pinfo.NetworkList {
+	//	ctx.devices.networkMap[nic.Index] = &InterfaceCreated{
+	//		Index:      nic.Index,
+	//		PCIAddr:    nic.PciAddr,
+	//		DeviceName: nic.DeviceName,
+	//		IpAddr:     nic.IpAddr,
+	//	}
+	//}
 
 	return ctx, nil
 }
