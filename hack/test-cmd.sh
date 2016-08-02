1d5cb511734cd05d13155b4afde6cbfcf607ffbd
Zhang Wei <zhangwei555@huawei.com>
2016-08-02 17:39:04 +0800

    Add basic integration framework
    Add basic framework for integration tests, also with some test cases for
    runv start, list, spec, state
    
    Signed-off-by: Zhang Wei <zhangwei555@huawei.com>

--- hack/test-cmd.sh
+++ hack/test-cmd.sh
@@ -7,9 +7,18 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# TODO directly test runv here
+HYPERSTART_COMMIT="d3cfa23ddeb43c5d99807c8db"
 
+# prepare kernel and initrd
+HYPERSTARTPATH="$GOPATH/src/github.com/hyperhq/hyperstart"
+RUNVPATH="$GOPATH/src/github.com/hyperhq/runv"
+cd $HYPERSTARTPATH && git checkout -q $HYPERSTART_COMMIT
+./autogen.sh && ./configure && make
+cp -v $HYPERSTARTPATH/build/{kernel,hyper-initrd.img} $RUNVPATH/integration-test/test_data/
 
+# do runv integration-test
+cd $RUNVPATH 
+make test-integration
 
 ###########################
 # test runv from hyper
@@ -25,3 +34,4 @@ cd ${GOPATH}/src/github.com/hyperhq/hyperd || cancel_and_exit "failed to find hy
 ./configure || cancel_and_exit "failed to configure hyper"
 make || cancel_and_exit "failed to compile hyper"
 hack/test-cmd.sh
+
---
+++ integration-test/utils.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"path/filepath"
+	"syscall"
+
+	"github.com/docker/docker/pkg/integration/checker"
+	"github.com/go-check/check"
+	"github.com/opencontainers/runtime-spec/specs-go"
+)
+
+// GetExitCode returns the ExitStatus of the specified error if its type is
+// exec.ExitError, returns 0 and an error otherwise.
+func GetExitCode(err error) (int, error) {
+	exitCode := 0
+	if exiterr, ok := err.(*exec.ExitError); ok {
+		if procExit, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+			return procExit.ExitStatus(), nil
+		}
+	}
+	return exitCode, fmt.Errorf("failed to get exit code")
+}
+
+// ProcessExitCode process the specified error and returns the exit status code
+// if the error was of type exec.ExitError, returns nothing otherwise.
+func ProcessExitCode(err error) (exitCode int) {
+	if err != nil {
+		var exiterr error
+		if exitCode, exiterr = GetExitCode(err); exiterr != nil {
+			// we've failed to retrieve exit code, so we set it to 127
+			exitCode = 127
+		}
+	}
+	return
+}
+
+func (s *RunVSuite) runvCommandWithError(args ...string) (string, int, error) {
+	cmdArgs := []string{"--kernel", s.kernelPath, "--initrd", s.initrdPath}
+	cmdArgs = append(cmdArgs, args...)
+	cmd := exec.Command(s.binaryPath, cmdArgs...)
+	out, err := cmd.CombinedOutput()
+	exitCode := ProcessExitCode(err)
+	return string(out), exitCode, err
+}
+
+func (s *RunVSuite) runvCommand(c *check.C, args ...string) (string, int) {
+	out, exitCode, err := s.runvCommandWithError(args...)
+	if c != nil {
+		c.Assert(err, checker.IsNil, check.Commentf("out: %s, exitCode: %d", out, exitCode))
+	}
+	return out, exitCode
+}
+
+func (s *RunVSuite) addSpec(spec *specs.Spec) error {
+	specData, err := json.MarshalIndent(spec, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	// write spec contents into file
+	s.configPath = filepath.Join(s.bundlePath, configFileName)
+	err = ioutil.WriteFile(s.configPath, specData, 0666)
+	if err != nil {
+		return err
+	}
+	return nil
+}
