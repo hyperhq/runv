3eb6c14318426cb838d0106f4cdf6b4b3212cf59
Lai Jiangshan <jiangshanlai@gmail.com>
2016-09-06 20:25:16 +0800

    remove test-from-hyperd
    Signed-off-by: Lai Jiangshan <jiangshanlai@gmail.com>

--- hack/test-cmd.sh
+++ hack/test-cmd.sh
@@ -7,31 +7,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-HYPERSTART_COMMIT="d3cfa23ddeb43c5d99807c8db"
-
 # prepare kernel and initrd
 HYPERSTARTPATH="$GOPATH/src/github.com/hyperhq/hyperstart"
 RUNVPATH="$GOPATH/src/github.com/hyperhq/runv"
-cd $HYPERSTARTPATH && git checkout -q $HYPERSTART_COMMIT
-./autogen.sh && ./configure && make
+cd $HYPERSTARTPATH && ./autogen.sh && ./configure && make
 cp -v $HYPERSTARTPATH/build/{kernel,hyper-initrd.img} $RUNVPATH/integration-test/test_data/
 
 # do runv integration-test
-cd $RUNVPATH 
+cd $RUNVPATH
 make test-integration
-
-###########################
-# test runv from hyper
-
-function cancel_and_exit()
-{
-	echo $1
-	exit 0 # don't fail in preparing hyper
-}
-
-cd ${GOPATH}/src/github.com/hyperhq/hyperd || cancel_and_exit "failed to find hyper"
-./autogen.sh || cancel_and_exit "failed to autogen hyper"
-./configure || cancel_and_exit "failed to configure hyper"
-make || cancel_and_exit "failed to compile hyper"
-hack/test-cmd.sh
 
