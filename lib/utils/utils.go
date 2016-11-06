799b16907e50ba310c67b8273662f2a2b52b5151
Peng Tao <bergwolf@gmail.com>
2016-11-07 00:59:46 +0800

    utils: add vsock dialer
    It only works for Linux.
    
    Signed-off-by: Peng Tao <bergwolf@gmail.com>

--- lib/utils/utils.go
+++ lib/utils/utils.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"net"
 	"time"
+
+	"github.com/hyperhq/runv/lib/vsock"
 )
 
 func DiskId2Name(id int) string {
@@ -17,6 +19,18 @@ func UnixSocketConnect(name string) (conn net.Conn, err error) {
 	for i := 0; i < 500; i++ {
 		time.Sleep(20 * time.Millisecond)
 		conn, err = net.Dial("unix", name)
+		if err == nil {
+			return
+		}
+	}
+
+	return
+}
+
+func VmSocketConnect(cid uint32, port uint32) (conn net.Conn, err error) {
+	for i := 0; i < 500; i++ {
+		time.Sleep(20 * time.Millisecond)
+		conn, err = vsock.Dial(cid, port)
 		if err == nil {
 			return
 		}
