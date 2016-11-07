c1c0671460641d052b9e36dffdaa008aa76eaea2
Peng Tao <bergwolf@gmail.com>
2016-11-07 17:45:13 +0800

    use net.Conn for guest serial port connection
    Abandon net.UnixConn so that it is possible to use vsock connection
    the same way.
    
    Signed-off-by: Peng Tao <bergwolf@gmail.com>

--- hypervisor/init_comm.go
+++ hypervisor/init_comm.go
@@ -82,7 +82,7 @@ func NewVmMessage(m *hyperstartapi.DecodedMessage) []byte {
 	return msg
 }
 
-func ReadVmMessage(conn *net.UnixConn) (*hyperstartapi.DecodedMessage, error) {
+func ReadVmMessage(conn net.Conn) (*hyperstartapi.DecodedMessage, error) {
 	needRead := 8
 	length := 0
 	read := 0
@@ -133,15 +133,15 @@ func waitInitReady(ctx *VmContext) {
 	if ctx.Boot.BootFromTemplate {
 		glog.Info("boot from template")
 		ctx.PauseState = PauseStatePaused
-		ctx.Hub <- &InitConnectedEvent{conn: conn.(*net.UnixConn)}
-		go waitCmdToInit(ctx, conn.(*net.UnixConn))
+		ctx.Hub <- &InitConnectedEvent{conn: conn}
+		go waitCmdToInit(ctx, conn)
 		// TODO call getVMHyperstartAPIVersion(ctx) after unpaused
 		return
 	}
 
 	glog.Info("Wating for init messages...")
 
-	msg, err := ReadVmMessage(conn.(*net.UnixConn))
+	msg, err := ReadVmMessage(conn)
 	if err != nil {
 		glog.Error("read init message failed... ", err.Error())
 		ctx.Hub <- &InitFailedEvent{
@@ -150,8 +150,8 @@ func waitInitReady(ctx *VmContext) {
 		conn.Close()
 	} else if msg.Code == hyperstartapi.INIT_READY {
 		glog.Info("Get init ready message")
-		ctx.Hub <- &InitConnectedEvent{conn: conn.(*net.UnixConn)}
-		go waitCmdToInit(ctx, conn.(*net.UnixConn))
+		ctx.Hub <- &InitConnectedEvent{conn: conn}
+		go waitCmdToInit(ctx, conn)
 		if !ctx.Boot.BootToBeTemplate {
 			getVMHyperstartAPIVersion(ctx)
 		}
@@ -174,7 +174,7 @@ func connectToInit(ctx *VmContext) {
 		return
 	}
 
-	go waitCmdToInit(ctx, conn.(*net.UnixConn))
+	go waitCmdToInit(ctx, conn)
 	getVMHyperstartAPIVersion(ctx)
 }
 
@@ -200,7 +200,7 @@ func getVMHyperstartAPIVersion(ctx *VmContext) error {
 	return nil
 }
 
-func waitCmdToInit(ctx *VmContext, init *net.UnixConn) {
+func waitCmdToInit(ctx *VmContext, init net.Conn) {
 	looping := true
 	cmds := []*hyperstartCmd{}
 
@@ -349,7 +349,7 @@ func waitCmdToInit(ctx *VmContext, init *net.UnixConn) {
 	}
 }
 
-func waitInitAck(ctx *VmContext, init *net.UnixConn) {
+func waitInitAck(ctx *VmContext, init net.Conn) {
 	for {
 		res, err := ReadVmMessage(init)
 		if err != nil {
