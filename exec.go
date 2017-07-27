3b7b341e3dfae5e2d0888f945d9263b35710b7bf
frank yang <yyb196@gmail.com>
2017-07-27 13:58:36 +0800

    Fixes #527 that mistook "init" for process name to monitor in case of exec command, which makes exec command whose process has exited stuck
    Signed-off-by: frank yang <yyb196@gmail.com>

--- exec.go
+++ exec.go
@@ -208,7 +208,7 @@ func runProcess(context *cli.Context, container string, config *specs.Process) (
 		monitorTtySize(c, container, process)
 	}
 
-	err = ociCreate(context, container, func(stdin, stdout, stderr string) error {
+	err = ociCreate(context, container, process, func(stdin, stdout, stderr string) error {
 		p := &types.AddProcessRequest{
 			Id:       container,
 			Pid:      process,
